ef68d9a65c955370647985c8f9211643bd1d6f84
Diego Gilson <diego.gilson@student.vinci.be>
2021-11-29 19:06:57 +0100

    ajout du navbar

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
 
+// Import Navbar
+import Navbar from "./Components/Navbar/Navbar";
+
 // This is the entry point to your app : add all relevant import and custom code
+
+Navbar();
+
+
 
