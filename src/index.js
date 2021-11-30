ff98aff493c75ea9f53e11a1aab58958c6eaed29
Diego Gilson <diego.gilson@student.vinci.be>
2021-11-30 16:55:10 +0100

    ajout pageHome

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import './stylesheets/main.css';
 
 // Import Navbar
 import Navbar from "./Components/Navbar/Navbar";
+import HomePage from './Components/Pages/HomePage';
 
 // This is the entry point to your app : add all relevant import and custom code
 
 Navbar();
+HomePage();
 
 
 
