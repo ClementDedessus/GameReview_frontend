2b904ff6f7fc9c859ca14740175087de88c61537
13Samael13 <roman.allion@student.vinci.be>
2021-12-02 13:16:06 +0100

    Ajout #page querySelector

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -1,7 +1,8 @@
 
 
 const HomePage = () => { 
-    const pageEntiere = document.querySelector("page");
+    const pageEntiere = document.querySelector("#page");
+    console.log("PAGE :" + document.querySelector("#page"));
     let home = `  <div class="container">
   <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
