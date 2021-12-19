2cb2a135070da69f4fe526e3ae366c5c6c36f5e9
leolamien <sienou.lamien@student.vinci.be>
2021-12-19 16:03:01 +0100

    refactor recommandation

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -25,7 +25,7 @@ const HomePage = async () => {
     console.error("Homepage::error: ", error);
   }
 
-  if(!vote){
+  if(!vote && user){
   try {
     const response = await fetch(`/api/liked/${user.username}`);
     if (!response.ok) {
