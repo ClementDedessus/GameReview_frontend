08ec9747a0efde9ecc037c0e4532b2545fa35091
leolamien <sienou.lamien@student.vinci.be>
2021-12-19 21:03:27 +0100

    refactor change username and password

--- src/Components/Pages/ProfilPage.js
+++ src/Components/Pages/ProfilPage.js
@@ -1,19 +1,36 @@
 import { getSessionObject } from "../utils/session";
+import { Redirect } from "../Router/Router";
 
 const profilpage = `
     <div class="text-center">
       <h3>Profil</h3>
     </div>
     <div class="text-left">
-        <ul>
-            <li><a href="http://localhost:8080/changeusername">Change username</a></li>
-            <li><a href="http://localhost:8080/changepassword">Change password</a></li>
-        </ul>
-    </div>`;
+        
+            <button type="submit" class="btn btn-outline-primary" id="btn1">Change username</button>
+            
+           
+        
+    </div>
+    <div class="text-left">
+        
+  
+    
+    <button type="submit" class="btn btn-outline-primary" id="btn2">Change password</button>
+
+</div>`;
 
 const ProfilPage = () => {
   let user = getSessionObject("user");
   const main = document.querySelector("#main");
   main.innerHTML = profilpage;
+  const username = document.getElementById("btn1");
+  const password = document.getElementById("btn2");
+  username.addEventListener("click", (event) => {
+    Redirect("/changeusername");
+  });
+  password.addEventListener("click", (event) => {
+    Redirect("/changepassword");
+  });
 };
 export default ProfilPage;
